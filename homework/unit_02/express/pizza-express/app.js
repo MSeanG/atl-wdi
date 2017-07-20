7ae7894da58d2d7f1e5788ec995e2f5f66c377cd
MSeanG <sgilmore@michael-sean.com>
2017-07-20 06:51:13 -0400

    commiting to make sure  i included all recent changes

--- homework/unit_02/express/pizza-express/app.js
+++ homework/unit_02/express/pizza-express/app.js
@@ -19,17 +19,18 @@ app.listen(port, function(){
   console.log("==========================")
 });
 
-app.get('/', function(req, res, next) {
-    res.send('Welcome to Pizza Express!');
+app.get('/', (req, res, next) => {
+    //res.send('Welcome to Pizza Express!');
+
 });
-app.get('/topping/:type', function(req, res, next) {
+app.get('/topping/:type', (req, res, next) => {
     var type = 'pepperoni';
-    res.send(`Topping ${type}`);
+    ''res.send(`Topping ${type}`);
 });
- app.get('/order/:amount/:size', function(req, res, next) {
+ app.get('/order/:amount/:size', (req, res, next) => {
     var amount = '10';
     var size = 'medium';
-    res.send(`Your order for ${amount} ${size} pizzas will be ready in 1 minute!`);
+    //res.send(`Your order for ${amount} ${size} pizzas will be ready in 1 minute!`);
  });
 //app.get('/favorite-foods', function(req, res) {
 //    var favoriteFoods = ["Jeni's almond butter brittle ice cream", 'Tacos from Superica', 'a breakfast sandwich from Gjelina to go in Venice', 'Croissants from Bottega Louie in downtown Los Angeles', 'Pizza from Little Star in San Francisco'];
