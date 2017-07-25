84d9fe5f031974914181c13aacd603f052a709a2
Sean <sgilmore@michael-sean.com>
2017-07-25 05:43:50 -0400

    promises lab read file

--- labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
+++ labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
@@ -1,22 +1,41 @@
 'use strict';
 
 const fs = require('fs');
-
+//console.log(process.argv)
 let inFile = process.argv[2];
-
-fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
-  if (error) {
-    console.error(error);
-  }
-
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
-
-  let lines = content.split('\n');
-
-  // clean up the array by removing the empty line
-  lines.pop();
-
-  lines.forEach(function(line) {
-    console.log('Hello, ' + line + '!');
+const readFilePromise = new Promise((resolve,reject)=>{
+  fs.readFile(inFile,{encoding: 'utf8'}, (err, content)=>{
+    if(err){
+      reject(err);
+    }
+    resolve(content);
   });
 });
+readFilePromise.then(data => {
+  let lines = data.split('\n');
+  lines.pop()
+  lines.forEach(function(line){
+    console.log('hello,' +line+'!');
+  });
+  .catch(err => {
+  console.log('there was an error');
+  console.log(err)
+  });
+});
+
+//fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+//  if (error) {
+//    console.error(error);
+//  }
+//
+//  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
+//
+//  let lines = content.split('\n');
+//
+//  // clean up the array by removing the empty line
+//  lines.pop();
+//
+//  lines.forEach(function(line) {
+//    console.log('Hello, ' + line + '!');
+//  });
+//});
