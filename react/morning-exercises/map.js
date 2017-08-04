d767bd419ed5bfa6abeb9795b9366cd4d6523c43
Sean <sgilmore@michael-sean.com>
2017-08-04 09:28:26 -0400

    .map() practice, 1st commit

---
+++ react/morning-exercises/map.js
@@ -0,0 +1,28 @@
+	const lyrics = ['never', 'gonna', 'give', 'you', 'up'];
+
+	const uppercaseLyrics = lyrics.map(function(nggyu) {
+    return nggyu.toUpperCase();
+  });
+  console.log(uppercaseLyrics);
+
+  
+  
+const people = [
+{
+  name: 'George Michael',
+  age: 14,
+  title: 'Mr. Manager'
+},
+{
+  name: 'T-Bone',
+  age: 34,
+  title: 'Arsonist'
+},
+{
+  name: 'George Oscar',
+  age: 32,
+  title: 'Illusionist'
+}
+];
+
+const names = 
