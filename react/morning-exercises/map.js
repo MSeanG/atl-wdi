7f451ac9c20a435d38e05f19bc52caf34f02faeb
Sean <sgilmore@michael-sean.com>
2017-08-04 10:12:50 -0400

    third commit for .map() practice

--- react/morning-exercises/map.js
+++ react/morning-exercises/map.js
@@ -8,24 +8,46 @@
   
   
 const people = [
-{
-  name: 'George Michael',
-  age: 14,
-  title: 'Mr. Manager'
-},
-{
-  name: 'T-Bone',
-  age: 34,
-  title: 'Arsonist'
-},
-{
-  name: 'George Oscar',
-  age: 32,
-  title: 'Illusionist'
-}
+  {
+    name: 'George Michael',
+    age: 14,
+    title: 'Mr. Manager'
+  },
+  {
+    name: 'T-Bone',
+    age: 34,
+    title: 'Arsonist'
+  },
+  {
+    name: 'George Oscar',
+    age: 32,
+    title: 'Illusionist'
+  }
 ];
 
 const names = people.map(function(x){
   return x.name;
 });
 console.log(names);
+
+
+
+const products = [
+  {
+    name: 'iPad',
+    price: 549.99
+  },
+  {
+    name: 'iPhone',
+    price: 799.99
+  },
+  {
+    name: 'iPod',
+    price: 2.99
+  }
+];
+
+const tax = products.map(function(x){
+  return x.price * .07;
+});
+console.log(tax);
