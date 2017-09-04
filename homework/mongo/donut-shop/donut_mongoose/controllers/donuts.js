58c7d996440979239f1d3e1e71f688367a7a7f82
Sean <sgilmore@michael-sean.com>
2017-09-04 10:28:44 -0400

    added new route

--- homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
@@ -7,8 +7,6 @@ var router = express.Router();
 
 var Donut = require("../models/donut");
 
-
-
 //======================
 // INDEX
 //======================
@@ -37,24 +35,12 @@ router.get('/new', (request, response) => {
 });
 
 
+
 //======================
 // SHOW
 //======================
 // Create a GET show route "/:id" that renders the donut's show page
-router.get('/:id', (request, response) => {
-    const donutIdToSearchDbFor = request.params.id;
 
-    Donut.findById(donutIdToSearchDbFor)
-        .then((donut) => {
-            response.render(
-                'donuts/show',
-                { donut }
-            );
-        })
-        .catch((error) => {
-            console.log(`Error retrieving donut with ID of ${donutIdToSearchDbFor}`)
-        });
-});
 
 
 
@@ -63,22 +49,6 @@ router.get('/:id', (request, response) => {
 //======================
 // Create a POST index route "/" that creates a new donut
 // and upon success redirects back to the index page "/"
-router.post('/', (request, response) => {
-
-    const newDonutInfoFromForm = request.body;
-
-
-    Donut.create(newDonutInfoFromForm)
-        .then((donut) => {
-            response.render(
-                'donuts/show',
-                { donut }
-            )
-        })
-        .catch((error) => {
-            console.log('Error saving new donut to database!');
-            console.log(error);
-        });
 
 
 
@@ -87,21 +57,6 @@ router.post('/', (request, response) => {
 //======================
 // Create a GET edit route "/:id/edit" that renders the edit.hbs page and
 // sends that donut's data to it
-router.get('/:id/edit', (request, response) => {
-
-    const donutIdToFind = request.params.id;
-
-    Donut.findById(donutIdToFind)
-        .then((donut) => {
-            response.render(
-                'donuts/edit',
-                { donut }
-            );
-        })
-        .catch((error) => {
-            console.log(`Error rendering edit form for donut with ID of ${donutIdToFind}`)
-        })
-});
 
 
 
@@ -110,30 +65,6 @@ router.get('/:id/edit', (request, response) => {
 //======================
 // Create a PUT update route "/:id" that updates the donut and
 // redirects back to the SHOW PAGE (not index)
-router.put('/:id', (request, response) => {
-
-    const donutIdToUpdate = request.params.id;
-    const updatedDonutInfo = request.body;
-
-    Donut.findByIdAndUpdate(
-        donutIdToUpdate,
-        updatedDonutInfo,
-        { new: true } // <-- DON'T FORGET THIS!!!
-    )
-        .then((donut) => {
-            console.log(`Donut with ID of ${donut._id} updated!`);
-
-            response.render(
-                'donut/show',
-                { donut }
-            )
-        })
-        .catch((error) => {
-            console.log(`Donut with ID of ${donut._id} failed to update!`)
-            console.log(error);
-        })
-
-});
 
 
 
@@ -142,21 +73,10 @@ router.put('/:id', (request, response) => {
 //======================
 // Create a DELETE delete route "/:id" that deletes the donut and
 // redirects back to index page "/"
-router.get('/:id/delete', (request, response) => {
 
-    const donutIdToDelete = request.params.id;
-
-    Donut.findByIdAndRemove(donutIdToDelete)
-        .then(() => {
-            console.log(`Successfully deleted donut with ID ${donutIdToDelete}!`)
-
-            response.redirect('/donut');
-        })
-});
 
 
 //======================
 // EXPORTS
 //======================
 // export router with module.exports
-module.exports = router;
